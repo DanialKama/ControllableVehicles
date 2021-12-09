b6d07b5167feb3a1bd8c318b3e683df4c2753dde
Danial Kama <DanialKamali777@gmail.com>
2021-12-09 14:34:43 +0330

    Game created
    A blank C++ project was created and some settings changed in project defaults

---
+++ Source/ControllableVehicles/ControllableVehiclesGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ControllableVehiclesGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CONTROLLABLEVEHICLES_API AControllableVehiclesGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/ControllableVehiclesEditor.Target.cs
@@ -0,0 +1,14 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class ControllableVehiclesEditorTarget : TargetRules
+{
+	public ControllableVehiclesEditorTarget( TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Editor;
+		DefaultBuildSettings = BuildSettingsVersion.V2;
+		ExtraModuleNames.AddRange( new string[] { "ControllableVehicles" } );
+	}
+}
